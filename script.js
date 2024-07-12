0284cdd2cf53582f2f904dc61b617602c0c7fe4a
Paulina Daciuk <71989308+paulinadaciuk03@users.noreply.github.com>
2024-07-12 09:24:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "53e844e741990ae136b2e0c6ddf1e21f";
+
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
